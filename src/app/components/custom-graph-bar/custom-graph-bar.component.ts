a146f544c775e697b4bef1511fee4f2ba7e5bd83
srod <srod>
2022-08-08 10:19:57 +0200

    add inputs to custom graph

--- src/app/components/custom-graph-bar/custom-graph-bar.component.ts
+++ src/app/components/custom-graph-bar/custom-graph-bar.component.ts
@@ -14,9 +14,36 @@ export declare interface IDataSource {
 export class CustomGraphBarComponent implements OnInit {
   /** Flag that displays spinner */
   public loading: boolean = false;
-  /**Data source of graph */
-  @Input() public dataSource: IDataSource[] = [];
-
+  /** The chart data */
+  @Input()
+  set dataSource(dataSource: IDataSource[]) {
+    this.loading = true;
+    this._dataSource = [...dataSource];
+    this.loading = false;
+  }
+  public _dataSource: IDataSource[] = [];
+  /** The dimensions of the chart [width, height]. If left undefined, the chart will fit to the parent container size */
+  @Input() public view: [number, number] = [1200, 800];
+  /** The legend title */
+  @Input() public legendTitle: string = '';
+  /** The x axis label text */
+  @Input() public xAxisLabel: string = '';
+  /** the y axis label text */
+  @Input() public yAxisLabel: string = '';
+  /** Show or hide legend */
+  @Input() public legend: boolean = true;
+  /** Show or hide the x axis label */
+  @Input() public showXAxisLabel: boolean = true;
+  /** Show or hide the y axis label */
+  @Input() public showYAxisLabel: boolean = true;
+  /** Show or hide the x axis */
+  @Input() public xAxis: boolean = true;
+  /** Show or hide the y axis */
+  @Input() public yAxis: boolean = true;
+  /** Fill elements with a gradient instead of a solid color */
+  @Input() public gradient: boolean = true;
+  /** Round edges for the bars*/
+  @Input() public roundEdges: boolean = true;
 
   constructor() { }
 
